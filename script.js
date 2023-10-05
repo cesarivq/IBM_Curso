8eaf9bc327122c200c9ccf69e218e53482d00dbe
Cesar <92646022+syntranet@users.noreply.github.com>
2023-10-05 16:55:33 -0600

    Fix the typo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Este es un comentario en JavaScript
 
 // Declaración de una variable
-var mensaje = "Hola, mundo!";
+var mensaje = "Hola, mundos!";
 
 // Muestra el mensaje en la consola del navegador
 console.log(mensaje);
