010192b2a557f1397741ceba2e1b1dd95860415c
cesarivq <80303558+cesarivq@users.noreply.github.com>
2023-10-05 17:58:44 -0500

    Revert "Fix the typo"

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Este es un comentario en JavaScript
 
 // Declaración de una variable
-var mensaje = "Hola, mundos!";
+var mensaje = "Hola, mundo!";
 
 // Muestra el mensaje en la consola del navegador
 console.log(mensaje);
